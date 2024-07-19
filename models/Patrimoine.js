f2b8d68acedcf2889e7910795c7fc0b6f9fbe21e
Naitive13 <hei.titan.2@gmail.com>
2024-07-19 12:15:52 +0300

    feat: check if possession belongs to this.possesseur

--- models/Patrimoine.js
+++ models/Patrimoine.js
@@ -1,18 +1,28 @@
-class Patrimoine {
+export default class Patrimoine {
   constructor(possesseur, date, possessions) {
-    this.possesseur  = possesseur;
-    this.date = date
-    this.possessions = possessions; // [Possession, Possession, ...]
+    this.possesseur = possesseur;
+    this.date = date;
+    this.possessions = [...possessions]; // [Possession, Possession, ...]
   }
   getValeur(date) {
-    return 
+    let result = 0;
+    for (const possession of this.possessions) {
+      result += possession.valeur;
+    }
+    return result;
   }
   addPossession(possession) {
-    this.possessions.push(possession);
+    if (possession.possesseur != this.possesseur) {
+      console.log(
+        `${possession.libelle} n'appartient pas à ${this.possesseur}`,
+      );
+    } else {
+      this.possessions.push(possession);
+    }
   }
   removePossession(possession) {
-    this.possessions = this.possessions.filter(p => p.libelle !== possession.libelle);
+    this.possessions = this.possessions.filter(
+      (p) => p.libelle !== possession.libelle,
+    );
   }
 }
-
-module.exports = Patrimoine;
