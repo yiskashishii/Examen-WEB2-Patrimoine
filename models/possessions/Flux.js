ddfadcfb7d3236ff5742a363e566ca4c9dcf2995
Abega1642 <hei.abega.2@gmail.com>
2024-08-02 21:34:33 +0300

    feat: implement getValeur method in Flux.js

--- models/possessions/Flux.js
+++ models/possessions/Flux.js
@@ -7,28 +7,47 @@ export default class Flux extends Possession {
   // Si salaire => +
   // Si train de vie => -
   constructor(possesseur, libelle, valeur, dateDebut, dateFin, tauxAmortissement, jour) {
+    super(possesseur, libelle, valeur, dateDebut, dateFin, tauxAmortissement)
     this.valeur = valeur;
     this.jour = jour;
     // this.source = source; // null || Compte
     // this.destination = destination; // Compte
     this.dateDebut = dateDebut;
     this.dateFin = dateFin;
+    this.valeurConstante = valeur
   }
+
 
   getValeur(date) {
     // TODO: calculer le montant total du flux en prenant compte du jour de versement
     // calcul différence entre date et date debut
-    const differenceDate = {
-      annee: date.getFullYear() - this.dateDebut.getFullYear(),
-      mois: date.getMonth() - this.dateDebut.getMonth(),
-    };
 
-    // calcul montant total
-    let valeurTotal = 0;
-    if (differenceDate.annee > 0 || differenceDate.mois > 0) {
-      valeurTotal = this.valeur * differenceDate.mois + this.valeur * differenceDate.annee * 12;
+    const prd = (debut, dateEvaluation, jourJ) => {
+        let start = new Date(debut);
+        let end = new Date(dateEvaluation);
+        let Dday = jourJ;
+        
+        let compteur = 0;
+    
+        if (start.getDate() <= Dday) {
+            compteur++;
+        }
+        
+        if (end.getDate() >= Dday && !(start.getFullYear() === end.getFullYear() && start.getMonth() === end.getMonth())) {
+            compteur++;
+        }
+        
+        let totalMois = (end.getFullYear() - start.getFullYear()) * 12 + (end.getMonth() - start.getMonth()) - 1;
+    
+        compteur += Math.max(0, totalMois);
+    
+        return compteur;
     }
 
-    return valeurTotal;
+    // calcul montant total
+
+    this.valeur += prd(this.dateDebut, date, this.jour.getDate()) * this.valeurConstante;
+
+    return this.valeur;
   }
 }
